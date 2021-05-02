5575f98c356e411cdd40e313c6ba330b3beaa389
Chan-ZJU <53956543+Chan-ZJU@users.noreply.github.com>
2021-05-02 14:24:46 +0800

    2

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
 var express = require("express")
-var mysql = require("mysql")
-var bodyParser = require("body-parser")
+//var bodyParser = require("body-parser")
 var app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.listen(3450,function(){
-    console.log("The server is running at port 3450")
+app.listen(3389,function(){
+    console.log("The server is running at port 3389")
 })
 
 
@@ -21,65 +20,13 @@ app.all("*", function(req, res, next) {
     next()
 })
 
-app.use("/interface",express.static('dist'))
-
 
 app.get("/device/addinfo",function(req,res){
-
-
-    var connection = mysql.createConnection({                //修改data数据库
-        host: 'localhost',
-        user: 'root',
-        password: '20020118czr',
-        database: 'data'
-    })
     var id = req.query.id
     var alcohol = req.query.alcohol
     var gps = req.query.gps
-    var instruction = "INSERT INTO data (id,alcohol,gps) VALUES(?,?,?);"
-    var params = [id,alcohol,gps]
-    connection.connect()
-    connection.query(instruction,params,function(err,result){
-        if(err){
-            console.log("-------ERROR!-------")
-            console.log(err.message)
-        }
-    })
-    connection.end()
+
+    res.send([id,alcohol,gps]);
 
 
-    var connection = mysql.createConnection({                   //修改device中的id 有则不管 无则插入
-       host: 'localhost',
-        user: 'root',
-        password: '20020118czr',
-        database: 'device'
-    })
-
-    var instruction3 = " insert into device (id) values(?) ON DUPLICATE KEY UPDATE id = "+id
-    connection.connect()
-    connection.query(instruction3,id,function(err,result){
-        if(err){
-            console.log("-------ERROR!-------")
-            console.log(err.message)
-        }
-    })
-    connection.end()
-
-
-    var connection = mysql.createConnection({                         //修改device表
-       host: 'localhost',
-        user: 'root',
-        password: '20020118czr',
-        database: 'device'
-    })
-
-    var instruction1 = "UPDATE device SET alcohol=?, gps=? WHERE id = " + id
-    connection.connect()
-    connection.query(instruction1,params,function(err,result){
-        if(err){
-            console.log("-------ERROR!-------")
-            console.log(err.message)
-        }
-    })
-    connection.end()
 })
